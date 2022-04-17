c4b6e0707afd070b0b2dc09a7d5ff45c3b7dd3d5
VerxivrEltaire <kenneth.iheme@gmail.com>
2022-04-17 18:15:04 +0100

    Project Commit

---
+++ PermMissingElem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace PermMissingElem {
+    class Solution {
+        public int solution(int[] A) {
+            var values = A.OrderBy(x => x).ToList();
+            var result = 0;
+
+            result = values.Count == 0 ? 1 : values.Count == 1 ? values[0] == 1 ? 2 : 1 : values.Count > 1 ? values[0] == 2 ? 1 : 0 : 0;
+
+            if (result == 0) {
+                for (int i = 0; i < values.Count - 1; i++) {
+                    if (values[i + 1] != values[i] + 1) {
+                        result = values[i] + 1;
+                        break;
+                    } else if (values[i + 1] == values[values.Count - 1]) {
+                        result = values[i + 1] + 1;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+
+        }
+    }
+
+    class MainClass {
+        public static void Main(string[] args) {
+            var sol = new Solution();
+            int[] values = { };
+            Console.WriteLine(sol.solution(values));
+        }
+    }
+}
